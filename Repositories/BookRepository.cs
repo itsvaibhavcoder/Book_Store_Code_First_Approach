e4ccdd2ba707c9be6f05706ad920f16ec97852c4
Vaibhav <sukalevaibhav53@gmail.com>
2025-02-04 00:13:27 +0530

    pagination and search functionality

--- Repositories/BookRepository.cs
+++ Repositories/BookRepository.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using BookStore.Data;
 using BookStore.Models;
+using System.Linq;
 namespace BookStore.Repositories
 {
     public class BookRepository : IBookRepository
@@ -13,10 +14,45 @@ namespace BookStore.Repositories
             _context = context;
         }
 
-        public async Task<IEnumerable<Book>> GetBooks()
+        //public async Task<IEnumerable<Book>> GetBooks()
+        //{
+        //    return await _context.Books.ToListAsync();
+        //}
+
+        public async Task<IEnumerable<Book>> GetBooks(string searchTerm, int pageNumber, int pageSize)
         {
-            return await _context.Books.ToListAsync();
+            var query = _context.Books.AsQueryable();
+
+            // Apply search filter if searchTerm is provided
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query = query.Where(b => b.Title.Contains(searchTerm) || b.Author.Contains(searchTerm));
+            }
+
+            // Get total count for pagination metadata (if needed)
+            int totalItems = await query.CountAsync();
+
+            // Apply pagination
+            var books = await query
+                .Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return books;
         }
+
+        public async Task<int> GetBooksCount(string searchTerm)
+        {
+            var query = _context.Books.AsQueryable();
+
+            if (!string.IsNullOrEmpty(searchTerm))
+            {
+                query = query.Where(b => b.Title.Contains(searchTerm) || b.Author.Contains(searchTerm));
+            }
+
+            return await query.CountAsync();
+        }
+
 
         public async Task<Book> GetBookById(int id)
         {
--- Repositories/IBookRepository.cs
+++ Repositories/IBookRepository.cs
@@ -5,7 +5,10 @@ namespace BookStore.Repositories
 {
     public interface IBookRepository
     {
-        Task<IEnumerable<Book>> GetBooks();
+        //Task<IEnumerable<Book>> GetBooks();
+        Task<IEnumerable<Book>> GetBooks(string searchTerm, int pageNumber, int pageSize);
+        Task<int> GetBooksCount(string searchTerm);
+
         Task<Book> GetBookById(int id);
         Task<Book> AddBook(Book book);
         Task<Book> UpdateBook(Book book);
